0ba09b44cdcef7331f457b892af55c2e12a5b44d
Vanbasten01 <fouad.yasin01@gmail.com>
2024-01-03 13:59:05 +0000

    Task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments,
+  };
+}
